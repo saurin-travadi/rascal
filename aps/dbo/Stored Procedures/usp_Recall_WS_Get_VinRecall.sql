851916618577df5272f467ba86df27cbd8fbc9a8
saurin.travadi@yahoo.com <saurin.travadi@yahoo.com>
2016-05-28 08:11:43 -0500

    recall data script

--- aps/dbo/Stored Procedures/usp_Recall_WS_Get_VinRecall.sql
+++ aps/dbo/Stored Procedures/usp_Recall_WS_Get_VinRecall.sql
@@ -13,19 +13,26 @@ BEGIN
     IF NOT EXISTS(SELECT 1 FROM [dbo].[Recall_VinReservation] (NOLOCK) WHERE [RecallData] IS NULL)
 	BEGIN
 
-		CREATE TABLE #Recall(VIN varchar(20),RecallData VARCHAR(100),CampNo VARCHAR(12), CompName VARCHAR(256))
+		CREATE TABLE #Recall(VIN varchar(20),Make VARCHAR(100), Model VARCHAR(100), ModelYear INT, RecallData VARCHAR(100), CampNo VARCHAR(50), CompName VARCHAR(256))
 
-		--Get recalldata='' (system marked, no recall found) for all the vins in VinInfo but not added to VinReservation
-		INSERT INTO #Recall(VIN,RecallData,CampNo, CompName)		
-		SELECT DISTINCT rv.VIN,rv.RecallData,CAMPNO,COMPNAME FROM 
-			(SELECT VIN,RecallData=item FROM [dbo].[Recall_VinReservation] CROSS APPLY dbo.SplitString(RecallData,'~') WHERE item IS NOT NULL) rv
-			JOIN [dbo].[Recall_Campaign] c ON c.[CAMPNO]=REPLACE(rv.[RecallData],'-','') 
-		UNION				
-			SELECT DISTINCT rv.VIN,rv.RecallData,CAMPNO,COMPNAME FROM 
-			(SELECT VIN,RecallData=item FROM [dbo].[Recall_VinReservation] CROSS APPLY dbo.SplitString(RecallData,'~') WHERE item IS NOT NULL) rv
-			JOIN [dbo].[Recall_Campaign] c ON c.[CAMPNO]=REPLACE(rv.[RecallData],'-','')+'000' 
-		UNION
-			SELECT VIN,'','','' FROM [dbo].[Recall_VinInfo] (NOLOCK) WHERE VIN NOT IN (SELECT VIN FROM [dbo].[Recall_VinReservation] (NOLOCK))
+		--Get all VINs not given to humans		
+		INSERT INTO #Recall(VIN, Make, Model, ModelYear, RecallData)
+		SELECT VIN,Make,Model,ModelYear,NULL FROM [dbo].[Recall_VinInfo] vi (NOLOCK) WHERE VIN NOT IN (SELECT VIN FROM [dbo].[Recall_VinReservation] (NOLOCK) WHERE LEN(RecallDatA)>1)
+
+		--Get all VINs given to humans
+		INSERT INTO #Recall(VIN, Make, Model, ModelYear, RecallData)
+		SELECT vi.VIN,Make,Model,ModelYear,Item FROM [dbo].[Recall_VinInfo] vi (NOLOCK)
+			JOIN (SELECT VIN,Item FROM [dbo].[Recall_VinReservation] vr (NOLOCK) CROSS APPLY dbo.SplitString(ISNULL(RecallData,''),'~') 
+				WHERE VIN IN (SELECT VIN FROM [dbo].[Recall_VinReservation] (NOLOCK) WHERE LEN(RecallDatA)>1)
+				AND Item IS NOT NULL
+			)vr ON vi.VIN=vr.VIN
+
+		UPDATE r SET CampNo=c.CAMPNO, CompName=c.COMPNAME
+			FROM #Recall r JOIN [dbo].[Recall_Campaign] c ON c.[CAMPNO]=REPLACE(r.[RecallData],'-','') AND c.MAKETXT=r.Make AND c.MODELTXT=r.Model AND c.YEARTXT=r.ModelYear
+
+		UPDATE r SET CampNo=c.CAMPNO, CompName=c.COMPNAME
+			FROM #Recall r JOIN [dbo].[Recall_Campaign] c ON c.[CAMPNO]=REPLACE(r.[RecallData],'-','')+'000' AND c.MAKETXT=r.Make AND c.MODELTXT=r.Model AND c.YEARTXT=r.ModelYear
+
 
 		--Map camp number to hollander number (Joe to provide spread sheet)
 		--map hollander number to LKQ number (Joe to provide spread sheet)
---
+++ aps/dbo/Tables/Recall_DirtyVIN.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Recall_DirtyVIN] (
+    [Id]          INT          IDENTITY (1, 1) NOT NULL,
+    [year]        INT          NULL,
+    [make]        VARCHAR (50) NULL,
+    [model]       VARCHAR (50) NULL,
+    [vin]         VARCHAR (20) NULL,
+    [source]      VARCHAR (10) NULL,
+    [recallfound] BIT          NULL,
+    [makefound]   BIT          NULL,
+    [modelfound]  BIT          NULL,
+    CONSTRAINT [PK_Recall_DirtyVIN] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
